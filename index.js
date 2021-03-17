ff454ed4e5c92edfa3bd16c6aa4f8e7644cb827f
unknown <1127363972@qq.com>
2021-03-17 17:52:38 +0800

    'update'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://www.bing.com'
 fetch('https://www.bing.com/HPImageArchive.aspx?idx=0&n=1&format=js&pid=HpEdgeAn&mkt=zh-cn')
   .then(res => res.json())
   .then(json => {
-    console.log(json)
+    // console.log(json)
     let data = json.images[0]
     const { url, copyright } = data
     updateWallPaper(BASE_URL + url, copyright)
