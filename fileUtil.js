de50ee05e24abf45f6e277065fb4e6971910492a
unknown <1127363972@qq.com>
2021-03-18 11:46:39 +0800

    fixed

--- fileUtil.js
+++ fileUtil.js
@@ -1,12 +1,28 @@
 const fs = require('fs');
+const README_PATH = 'README.md'
 
-function updateWallPaper (url, copyright) {
-  let md = 
-  `
-  ![image](${url})[${copyright}](${url})
-  `
-  fs.appendFile('README.md', md, (err) => {
+function getToday () {
+  let T = new Date(),
+    year = T.getFullYear(),
+    month = T.getMonth() + 1,
+    day = T.getDate();
+  month = month >= 10 ? month : '0' + month
+  day = day > 10 ? day : '0' + day
+  return `${year}-${month}-${day}`
+}
+
+function updateWallPaper(url, copyright) {
+  fs.readFile(README_PATH, {
+    encoding: 'utf-8',
+    flag: 'r+'
+  }, (err, data) => {
     if (err) throw err;
+    console.log('data', data)
+    let day = getToday()
+    let md = `## ${day}\n![image](${url})[${copyright}](${url})\n`
+    fs.writeFile(README_PATH, md + data, (err) => {
+      if (err) throw err;
+    })
   })
 }
 
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const fetch = require('node-fetch');
-const { updateWallPaper } = require('./fileUtil.js');
+const schedule = require('node-schedule');
+const {
+  updateWallPaper
+} = require('./fileUtil.js');
 const BASE_URL = 'https://www.bing.com'
 
-fetch('https://www.bing.com/HPImageArchive.aspx?idx=0&n=1&format=js&pid=HpEdgeAn&mkt=zh-cn')
-  .then(res => res.json())
-  .then(json => {
-    // console.log(json)
-    let data = json.images[0]
-    const { url, copyright } = data
-    updateWallPaper(BASE_URL + url, copyright)
-  })
+
+const job = schedule.scheduleJob('* * 8 * * *', fetchData)
+
+function fetchData() {
+  fetch('https://www.bing.com/HPImageArchive.aspx?idx=0&n=1&format=js&pid=HpEdgeAn&mkt=zh-cn')
+    .then(res => res.json())
+    .then(json => {
+      // console.log(json)
+      let data = json.images[0]
+      const {
+        url,
+        copyright
+      } = data
+      updateWallPaper(BASE_URL + url, copyright)
+    })
+}
