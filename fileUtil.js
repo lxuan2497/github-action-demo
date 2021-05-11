7b130f2c9863fbb298b5d6ca2ba532d6bc1f3bb8
sep7th <48083512+lonzo97@users.noreply.github.com>
2021-05-11 11:04:22 +0800

    Update fileUtil.js

--- fileUtil.js
+++ fileUtil.js
@@ -7,7 +7,7 @@ function getToday () {
     month = T.getMonth() + 1,
     day = T.getDate();
   month = month >= 10 ? month : '0' + month
-  day = day > 10 ? day : '0' + day
+  day = day >= 10 ? day : '0' + day
   return `${year}-${month}-${day}`
 }
 
